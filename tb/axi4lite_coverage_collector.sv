0728ad40155e7bdae8e6d64b9fe0f93ba2d379b9
Snowy <95209011+Snowy0521@users.noreply.github.com>
2026-09-05 23:22:57 +0200

    Implement conditional coverage reporting
    Add coverage reporting in report_phase function.

--- tb/axi4lite_coverage_collector.sv
+++ tb/axi4lite_coverage_collector.sv
@@ -63,9 +63,11 @@ class axi4lite_coverage_collector extends uvm_subscriber #(axi4lite_txn);
 
     function void report_phase(uvm_phase phase);
         super.report_phase(phase);
+    `ifndef
         `uvm_info("COVERAGE",
             $sformatf("Coverage for %s: %0.2f%%", get_full_name(), cg_axi4lite.get_coverage()),
             UVM_LOW)
+     `endif
     endfunction
 endclass
 
